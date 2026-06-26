166cae44ef3c9a1e7093eabcb39db2c7b4e67521
Mirella Costa <mirellabcost@gmail.com>
2026-06-26 16:50:40 -0300

    added a method to check if corresponds to filter and to filter per category

--- RecommendedContentList.hpp
+++ RecommendedContentList.hpp
@@ -4,11 +4,13 @@
 class RecommendedContentList {
 private:
     DoublyNode* head;
+    bool correspondsToFilter(const Content& content, const std::string& category) const;
 
 public:
     RecommendedContentList() : head(nullptr) {}
 
     void addContent(const Content& content);
+    RecommendedContentList filterPerCategory(const std::string& category) const;
     void removeContent(int id);
     void display() const;
 };
